b22d570dfbf9b1ca604095c1cd03138242d4cbda
Ludovic Muller <ludovicmuller1@gmail.com>
2021-01-17 13:00:46 +0100

    update comments

--- src/Strings.php
+++ src/Strings.php
@@ -2,10 +2,18 @@
 
 namespace ludovicm67\Strings;
 
+/**
+ * Strings is a class to manipulate strings easily
+ */
 class Strings {
 
-  // Clean a string
-  static function clean($string) {
+  /**
+   * Clean a string
+   *
+   * @param string $string the string to clean
+   * @return string cleaned string
+   */
+  static function clean(string $string): string {
     return stripslashes(
       trim(
         htmlspecialchars(
@@ -17,8 +25,13 @@ class Strings {
     );
   }
 
-  // Unconvert a string from camelCase
-  static function fromCamelCase($string) {
+  /**
+   * Unconvert a string from camelCase
+   *
+   * @param string $string the string to unconvert
+   * @return string unconverted string
+   */
+  static function fromCamelCase(string $string): string {
     return strtolower(
       preg_replace(
         ['/([a-z\d])([A-Z])/', '/([^-])([A-Z][a-z])/'],
@@ -27,8 +40,13 @@ class Strings {
     );
   }
 
-  // Convert a string to camelCase
-  static function toCamelCase($string) {
+  /**
+   * Convert a string to camelCase
+   *
+   * @param string $string the string to convert to camelCase
+   * @return string string converted to camelCase
+   */
+  static function toCamelCase(string $string): string {
     return lcfirst(
       str_replace(' ', '', ucwords(
         str_replace('-', ' ', self::fromCamelCase($string))
