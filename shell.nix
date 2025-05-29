c90f082753f60873d815b566483bd512c616ab3e
Ludovic Muller <ludovicmuller1@gmail.com>
2025-05-29 09:02:04 +0200

    chore: upgrade phpunit to v12

--- shell.nix
+++ shell.nix
@@ -1,8 +1,21 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [
-    git
-    php84
-    php84Packages.composer
-  ];
+
+let
+  php = pkgs.php84.buildEnv {
+    extensions = { enabled, all }:
+      with all; [
+        xdebug
+        dom
+        mbstring
+        tokenizer
+        xmlwriter
+      ];
+  };
+
+in pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ git php php.packages.composer ];
+
+  shellHook = ''
+    export XDEBUG_MODE=coverage
+  '';
 }
